f774846885d8d7eec8bd07cdfb547761a4594ecf
Santhoshkumard11 <santhoshkdhana@gmail.com>
2022-03-26 13:45:47 +0530

    added open brower methods to be accesed for teams

---
+++ python/_helper.py
@@ -0,0 +1,29 @@
+import os
+import webbrowser
+
+
+def execute_on_shell(command_text: str):
+    os.system(command_text)
+
+
+def open_browser(url: str):
+    webbrowser.open(url, new=2, autoraise=True)
+
+
+def create_requirements():
+    execute_on_shell("pip freeze > requirements.txt")
+
+
+def open_teams_chat(email: str):
+    url_teams_chat = f"https://teams.microsoft.com/l/chat/0/0?users={email}"
+    open_browser(url_teams_chat)
+
+
+def call_on_teams(email: str):
+    url_call_on_teams = f"https://teams.microsoft.com/l/call/0/0?users={email}"
+    open_browser(url_call_on_teams)
+
+
+def open_mail(email: str):
+    url_mail_people = f"mailto:{email}"
+    open_browser(url_mail_people)
